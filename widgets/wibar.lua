2fdf6584815ab07ec7b7158a8393f3337a8dae0f
Mohammad Ghazanfari <mgh.5225@gmail.com>
2022-12-15 15:24:47 +0330

    code clean up

--- widgets/wibar.lua
+++ widgets/wibar.lua
@@ -3,18 +3,66 @@ local wibox = require("wibox")
 local beautiful = require("beautiful")
 
 local menu = require("widgets.menu")
-
-
-Mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
-    menu = menu.mymainmenu })
-
--- Keyboard map indicator and switcher
 local keyboard = require("widgets.keyboard")
 
--- Create a textclock widget
-Mytextclock = wibox.widget.textclock()
 
 return function(s)
+    local mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
+        menu = menu.mymainmenu })
+
+    -- Create a textclock widget
+    local mytextclock = wibox.widget.textclock()
+    -- Create a promptbox for each screen
+    local mypromptbox = awful.widget.prompt()
+
+    -- Create an imagebox widget which will contain an icon indicating which layout we're using.
+    -- We need one layoutbox per screen.
+    local mylayoutbox = awful.widget.layoutbox {
+        screen  = s,
+        buttons = {
+            awful.button({}, 1, function() awful.layout.inc(1) end),
+            awful.button({}, 3, function() awful.layout.inc(-1) end),
+            awful.button({}, 4, function() awful.layout.inc(-1) end),
+            awful.button({}, 5, function() awful.layout.inc(1) end),
+        }
+    }
+
+    -- Create a taglist widget
+    local mytaglist = awful.widget.taglist {
+        screen  = s,
+        filter  = awful.widget.taglist.filter.all,
+        buttons = {
+            awful.button({}, 1, function(t) t:view_only() end),
+            awful.button({ Modkey }, 1, function(t)
+                if client.focus then
+                    client.focus:move_to_tag(t)
+                end
+            end),
+            awful.button({}, 3, awful.tag.viewtoggle),
+            awful.button({ Modkey }, 3, function(t)
+                if client.focus then
+                    client.focus:toggle_tag(t)
+                end
+            end),
+            awful.button({}, 4, function(t) awful.tag.viewprev(t.screen) end),
+            awful.button({}, 5, function(t) awful.tag.viewnext(t.screen) end),
+        }
+    }
+
+    -- Create a tasklist widget
+    local mytasklist = awful.widget.tasklist {
+        screen  = s,
+        filter  = awful.widget.tasklist.filter.currenttags,
+        buttons = {
+            awful.button({}, 1, function(c)
+                c:activate { context = "tasklist", action = "toggle_minimization" }
+            end),
+            awful.button({}, 3, function() awful.menu.client_list { theme = { width = 250 } } end),
+            awful.button({}, 4, function() awful.client.focus.byidx(-1) end),
+            awful.button({}, 5, function() awful.client.focus.byidx(1) end),
+        }
+    }
+
     awful.wibar {
         position = "top",
         screen   = s,
@@ -22,17 +70,17 @@ return function(s)
             layout = wibox.layout.align.horizontal,
             { -- Left widgets
                 layout = wibox.layout.fixed.horizontal,
-                Mylauncher,
-                s.mytaglist,
-                s.mypromptbox,
+                mylauncher,
+                mytaglist,
+                mypromptbox,
             },
-            s.mytasklist, -- Middle widget
+            mytasklist, -- Middle widget
             { -- Right widgets
                 layout = wibox.layout.fixed.horizontal,
                 keyboard.widget,
                 wibox.widget.systray(),
-                Mytextclock,
-                s.mylayoutbox,
+                mytextclock,
+                mylayoutbox,
             },
         }
     }
